b4184de36edded537cd1b0c49f368b23971aca64
spymanian <90417628+spymanian@users.noreply.github.com>
2023-01-11 18:55:53 -0800

    Modified AIFollow

--- Cogschan Project/Assets/EnemyAIBasic/AIFollow.cs
+++ Cogschan Project/Assets/EnemyAIBasic/AIFollow.cs
@@ -8,15 +8,28 @@ public class AIFollow : MonoBehaviour
     public float lookRadius = 10f;
     public NavMeshAgent nav;
     public Transform Player;
+    public float idleTime = 10f;
+    private float currentTime;
+
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        currentTime = idleTime;
     }
 
-    void Update()
-    {
+    void Update(){
         nav.SetDestination(Player.position);
         transform.LookAt(Player);
+        if (nav.remainingDistance == 0){
+            currentTime -= Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                nav.destination = Player.position;
+                currentTime = idleTime;
+            }
+        }else{
+            currentTime = idleTime;
+        }
 
     }
     void OnDrawGizmosSelected ()
